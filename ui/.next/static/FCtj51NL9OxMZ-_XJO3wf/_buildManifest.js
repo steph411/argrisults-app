bc526a4556098c1a45938c2cf529861a6f8150a2
Lionel ETABA <stephane.etaba@bara.ca>
2020-11-19 10:07:08 +0100

    updated map styles

---
+++ ui/.next/static/FCtj51NL9OxMZ-_XJO3wf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F2c796e83.197011c0d8d214b4532e.js","static\u002Fchunks\u002Fpages\u002Findex-3f38fb00f9d3a560021e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5eb0f6ddbbaf7ef74547.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
